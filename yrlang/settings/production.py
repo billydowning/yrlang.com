9e92681c6eeddef306bddab9c221d54347231f15
yogendra.katewa <yogendra.katewa@trootech.com>
2020-07-01 11:21:48 +0530

    databse confihuration added for production server

--- yrlang/settings/production.py
+++ yrlang/settings/production.py
@@ -5,3 +5,14 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['18.216.99.152']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yrlang2020',
+        'USER': 'postgres',  # Do not change. Create a separate settings file for local dev if needs to be different
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
