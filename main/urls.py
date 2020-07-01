54a6470aab6107604b34717fe5195b5516d93047
akashvns <akashsuthar232@gmail.com>
2020-07-01 15:40:57 +0530

    explore page[added]

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from main import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('index', views.IndexView.as_view(), name='index'),
+    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.IndexView.as_view(), name='index'),
     path('search/', views.Search.as_view(), name='search'),
     path('time-zone/', views.TimeZone.as_view(), name='time_zone'),
     path('provider/', views.ProviderListsView.as_view(), name='provider_list'),
