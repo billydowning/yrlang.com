d949868f5665852609533ab23bb3a92ae4beca4e
Yogendra <yogendra.katewa@trootech.com>
2020-08-28 11:11:34 +0530

    Changed redirectign page from join our plattform

--- users/views.py
+++ users/views.py
@@ -446,18 +446,18 @@ class CreateRequestForLanguageVerifier(UserSessionAndLoginCheckMixing, UserPasse
         return self.render_to_response(self.get_context_data(form=form))
 
 
-class BecomeProviderView(UserSessionAndLoginCheckMixing, UserPassesTestMixin, TemplateView):
+class BecomeProviderView(TemplateView):
     template_name = 'users/become_provider.html'
 
-    def test_func(self):
-        return self.request.user.is_client_user(self.request.session.get('user_role'))
+    # def test_func(self):
+    #     return self.request.user.is_client_user(self.request.session.get('user_role'))
 
 
-class BecomeLocaliteView(UserSessionAndLoginCheckMixing, UserPassesTestMixin, TemplateView):
+class BecomeLocaliteView(TemplateView):
     template_name = 'users/become_localite.html'
 
-    def test_func(self):
-        return self.request.user.is_client_user(self.request.session.get('user_role'))
+    # def test_func(self):
+    #     return self.request.user.is_client_user(self.request.session.get('user_role'))
 
 
 class AboutUsView(TemplateView):
